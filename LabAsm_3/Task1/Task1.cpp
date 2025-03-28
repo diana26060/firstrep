db18eb5d11ddeed1d55385c7a8153d983a9362aa
diana26060 <345dtfgvh@gmail.com>
2025-03-28 19:20:17 +0300

    .

---
+++ LabAsm_3/Task1/Task1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+int main() {
+    int num, den, gcd;
+
+    std::cout << "Enter numerator: ";
+    std::cin >> num;
+    std::cout << "Enter denomenator: ";
+    std::cin >> den;
+    if (den <= 0) {
+        std::cout << "Error";
+        return 1;
+    }
+
+    int orig_num = num;
+    int orig_den = den;
+
+    _asm {
+        mov eax, orig_num
+        mov ecx, orig_den
+
+        gcd_loop :
+        test ecx, ecx
+            je done
+
+            xchg eax, ecx
+            xor edx, edx
+            div ecx
+            mov eax, ecx
+            mov ecx, edx
+            jmp gcd_loop
+
+            done :
+        mov gcd, eax
+
+    }
+
+    num = orig_num;
+    den = orig_den;
+
+    num /= gcd;
+    den /= gcd;
+
+    std::cout << "Result: " << num << "/" << den << std::endl;
+
+    return 0;
+}
---
+++ LabAsm_3/Task2/Task2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 0;
+    int x, y;
+
+    __asm {
+        mov count, 0
+        mov x, 1
+
+        start_x:
+            mov y, 1
+
+        start_y :
+            mov eax, 2
+            imul eax, x
+            mov ebx, 3
+            imul ebx, y
+            add eax, ebx
+            cmp eax, 50
+            je found
+            jmp next_y
+
+        found :
+            inc count
+
+        next_y :
+            inc y
+            cmp y, 50
+            jl start_y
+
+        next_x :
+            inc x
+            cmp x, 50
+            jl start_x
+    }
+
+    cout << "Amount of solutions: " << count << endl;
+
+    for (x = 1; x < 50; x++) {
+        for (y = 1; y < 50; y++) {
+            if (2 * x + 3 * y == 50) {
+                cout << "2*" << x << " + 3*" << y << " = 50" << endl;
+            }
+        }
+    }
+    return 0;
+}
